adf09d8850318293ea715fd2800112f5298b1c55
Timothy Shores <timothymshores@gmail.com>
2019-05-15 16:03:39 -0500

    Wrote test to check that the gate defaults to unlocked and open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,27 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
 import {
     render,
     fireEvent,
     cleanup,
     waitForElement,
-} from 'react-testing-library'
+} from 'react-testing-library';
+
+import renderer from 'react-test-renderer';
+
+import 'jest-dom/extend-expect'
 
 afterEach(cleanup)
 
 describe('<Dashboard />', () => {
+    it('makes the snapshot', () => {
+        expect(renderer.create(<Dashboard />).toJSON()).toMatchSnapshot();
+    });
     it('defaults to unlocked and open', () => {
-
+        const { getByText } = render(<Dashboard />);
+        expect(getByText(/unlocked/i)).toBeInTheDocument();
+        expect(getByText(/open/i)).toBeInTheDocument();
     });
     it('shows the controls and display', () => {
 
