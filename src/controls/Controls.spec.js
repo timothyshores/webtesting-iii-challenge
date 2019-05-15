679a0de699f4e15913e65dcecc7886ea1e7f917d
Timothy Shores <timothymshores@gmail.com>
2019-05-15 17:50:06 -0500

    Added boilerplate tests to Controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -6,4 +6,16 @@ describe('<Controls />', () => {
     it('makes the snapshot', () => {
         expect(renderer.create(<Controls />).toJSON()).toMatchSnapshot();
     });
+    it('provide buttons to toggle the closed and locked states', () => {
+
+    });
+    it("buttons' text changes to reflect the state the door will be in if clicked", () => {
+
+    });
+    it('the closed toggle button is disabled if the gate is closed', () => {
+
+    });
+    it('the locked toggle button is disabled if the gate is open', () => {
+
+    });
 });
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -4,34 +4,34 @@ import Display from '../display/Display';
 import Controls from '../controls/Controls';
 
 class Dashboard extends React.Component {
-  state = {
-    locked: false,
-    closed: false,
-  };
+    state = {
+        locked: false,
+        closed: false,
+    };
 
-  render() {
-    const { closed, locked } = this.state;
+    render() {
+        const { closed, locked } = this.state;
 
-    return (
-      <>
-        <Display locked={locked} closed={closed} />
-        <Controls
-          locked={locked}
-          closed={closed}
-          toggleLocked={this.toggleLocked}
-          toggleClosed={this.toggleClosed}
-        />
-      </>
-    );
-  }
+        return (
+            <>
+                <Display locked={locked} closed={closed} />
+                <Controls
+                    locked={locked}
+                    closed={closed}
+                    toggleLocked={this.toggleLocked}
+                    toggleClosed={this.toggleClosed}
+                />
+            </>
+        );
+    }
 
-  toggleLocked = () => {
-    this.setState(prev => ({ locked: !prev.locked }));
-  };
+    toggleLocked = () => {
+        this.setState(prev => ({ locked: !prev.locked }));
+    };
 
-  toggleClosed = () => {
-    this.setState(prev => ({ closed: !prev.closed }));
-  };
+    toggleClosed = () => {
+        this.setState(prev => ({ closed: !prev.closed }));
+    };
 }
 
 export default Dashboard;
