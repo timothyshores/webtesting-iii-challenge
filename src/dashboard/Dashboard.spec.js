17d4ca577753f093e35ac7f4686475bcf17abce7
Timothy Shores <timothymshores@gmail.com>
2019-05-15 15:23:23 -0500

    Install react-test-renderer as a dev dependency by running yarn add react-test-renderer --dev

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import {
+    render,
+    fireEvent,
+    cleanup,
+    waitForElement,
+} from 'react-testing-library'
+
+afterEach(cleanup)
+
+describe('<Dashboard />', () => {
+    it('defaults to unlocked and open', () => {
+
+    });
+    it('shows the controls and display', () => {
+
+    });
+});
